b1812bb9d4c9e775c9a0dbd6bee127228910d6c2
Evgeniy Evzerov <zevzerov@yandex.ru>
2018-01-30 01:36:52 +0300

    added: h2 db, liquibase; also some test data and functionality

---
+++ src/main/java/ru/benzoback/library/configuration/DBConfig.java
@@ -0,0 +1,43 @@
+package ru.benzoback.library.configuration;
+
+import liquibase.integration.spring.SpringLiquibase;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DBConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(false)
+                .setName("testdb")
+                .setType(EmbeddedDatabaseType.H2)
+                .addDefaultScripts()
+                .setScriptEncoding("UTF-8")
+                .ignoreFailedDrops(true)
+                .build();
+    }
+
+    @Bean
+    public NamedParameterJdbcTemplate namedParameterJdbcTemplate() {
+        return new NamedParameterJdbcTemplate(dataSource());
+    }
+
+    @Bean
+    public SpringLiquibase liquibase() {
+
+        SpringLiquibase springLiquibase = new SpringLiquibase();
+
+        springLiquibase.setDataSource(dataSource());
+        springLiquibase.setChangeLog("classpath:h2.changelog.xml");
+
+        return springLiquibase;
+    }
+
+}
--- src/main/java/ru/benzoback/library/controller/MainController.java
+++ src/main/java/ru/benzoback/library/controller/MainController.java
@@ -1,15 +1,19 @@
 package ru.benzoback.library.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.Arrays;
 
 @RequestMapping("/")
 @Controller
 public class MainController {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public String test() {
+    public String test(Model model) {
+        model.addAttribute("kek", "HELLO");
         return "test";
     }
 }
